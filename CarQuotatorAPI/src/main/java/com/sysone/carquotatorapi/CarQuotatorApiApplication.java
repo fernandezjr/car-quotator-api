f897dea3a63b03ba5e7a0397a515f5b9be313b31
Rodrigo Fernández <rfernandez@exisoft.com.ar>
2020-08-01 13:17:58 -0300

    Creación del proyecto Spring Boot

---
+++ CarQuotatorAPI/src/main/java/com/sysone/carquotatorapi/CarQuotatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.sysone.carquotatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarQuotatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarQuotatorApiApplication.class, args);
+	}
+
+}
---
+++ CarQuotatorAPI/src/test/java/com/sysone/carquotatorapi/CarQuotatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysone.carquotatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarQuotatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
